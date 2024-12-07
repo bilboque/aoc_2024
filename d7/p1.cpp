fccf8c1e1edf0cbc8b45c1d6d876a8dfb0f6261f
Leo Bonaz <leo-francois.bonaz@etu.unige.ch>
2024-12-07 11:31:58 +0100

    d7

---
+++ d7/p1.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll)x.size()
+
+//--------CODE--------//
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+#define MUL 1
+#define ADD 2
+#define CAT 3
+
+bool solve(ll goal, vector<ll> &arr, ll i, int op, ll curr) {
+    if (op == ADD)
+        curr += arr[i];
+    else if (op == MUL)
+        curr *= arr[i];
+
+    if (i+1 == arr.size())
+        return goal == curr;
+
+    return solve(goal, arr, i + 1, ADD, curr) ||
+           solve(goal, arr, i + 1, MUL, curr);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string eq;
+    ll sum = 0;
+    while (getline(cin, eq)) {
+        istringstream line(eq);
+
+        ll goal;
+        char _;
+        ll num;
+        line >> goal;
+        line >> _;
+
+        vector<ll> arr;
+
+        while (line >> num) arr.push_back(num);
+
+        if (solve(goal, arr, 0, ADD, 0))
+            sum += goal;
+   }
+    cout << sum << endl;
+}
